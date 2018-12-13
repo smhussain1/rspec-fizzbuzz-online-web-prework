1594991954f56a28e6e2be99f4e61de99e7be655
michael hussain <smhussain_1@yahoo.com>
2018-12-13 16:08:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+
+def fizzbuzz(integer)
+  if fizzbuzz(3) % 3 == 0  
+    return "Fizz"
+  end
+  
+  if fizz_5
+    return "Buzz"
+  end
+  if fizz_15 ==fizzbuzz(15)
+    return "Fizzbuzz"
+  end
+  if fizz_4 == fizzbuzz(4)
+    return "nil"
+  end
+end
